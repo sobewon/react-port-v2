f0e7ae611af7a165a1518dbd508ffd69351e52a0
John Magnuson <sobewon@gmail.com>
2023-05-31 20:12:40 -0400

    init cloned commit

---
+++ src/components/project.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import anime from '../assets/anime.png'
+import hobbybox from '../assets/hobbybox.jpeg'
+import weather from '../assets/weather-forecast.png'
+import planner from '../assets/work-planner.png'
+import yotsubaOld from '../assets/yotsubaFirst.png'
+import note from '../assets/notetaker.png'
+
+function Project() {
+  const projects = [
+    {
+      title: 'Anime Explorer',
+      imageSrc: anime,
+      deployedLink: 'https://prestontravis.github.io/Anime_Explorer/',
+      githubLink: 'https://github.com/prestontravis/Anime_Explorer',
+    },
+    {
+      title: 'HobbyBox',
+      imageSrc: hobbybox,
+      deployedLink: 'https://hobbybox.herokuapp.com/',
+      githubLink: 'https://github.com/Qaizen/hobby-box',
+    },
+    {
+      title: 'Track Z Weather',
+      imageSrc: weather,
+      deployedLink: 'https://sobewon.github.io/trackZweather/',
+      githubLink: 'https://github.com/sobewon/trackZweather',
+    },
+    {
+      title: 'Daily Planner',
+      imageSrc: planner,
+      deployedLink: 'https://sobewon.github.io/dailyPlanner/',
+      githubLink: 'https://github.com/sobewon/dailyPlanner',
+    },
+    {
+      title: 'Note Taker',
+      imageSrc: note,
+      deployedLink: 'https://note-taker-jm.herokuapp.com/',
+      githubLink: 'https://github.com/sobewon/NoteTaker',
+    },
+    {
+      title: 'My Very First HTML Page',
+      imageSrc: yotsubaOld,
+      deployedLink: 'https://sobewon.github.io/JMPortfolio/',
+      githubLink: 'https://github.com/sobewon/JMPortfolio',
+    },
+  ];
+
+  return (
+    <div className="project-page">
+      {projects.map((project, index) => (
+        <div key={index} className="project-item">
+          <h2>{project.title}</h2>
+          <a href={project.deployedLink} target="_blank" rel="noopener noreferrer">
+            <img src={project.imageSrc} alt={project.title} />
+          </a>
+          <a href={project.githubLink} target="_blank" rel="noopener noreferrer"> Repo Link</a>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Project;
